a759ab3d7bc168e20fa07e167a0922398607e1a6
charles.reitz <charles.reitz@c09b9852-311b-11de-a1c6-114bfcb2beae>
2009-05-11 01:03:21 +0000

    Criado Index juntamente com template que foi modifica para se adequar ao sistema, codigo para chamar links foi feito. Css modificada conforme template. Feito ajustes no cadastro de protocolos inserido o deletar para deletar todo o protocolo. Criado o arquivo default.css ao qual estão todas as css, falta retirar as css dos demais arquivos para que apenas peguem os parametros do default. Criado arquivo home.php

--- www/cadastroProtocolo.php
+++ www/cadastroProtocolo.php
@@ -1,60 +1,11 @@
 <?php
-session_start();
-?>
-
-<style>
-
-
-input{
-border-style:solid;
-border-color:black;
-border-width:1px;
-font-family:verdana;
-font-size:12px;
-}
-label, select, table{
-font-family:verdana;
-font-size:12px;
-}
-
- .descCampo{
-    background-color:silver;
-    font-weight:bold;
-
-}
-
-.resultCampo{
-    padding-bottom:5;
-}
-
-.tabItemProtocolo{
-    border:1px;
-    border-style:solid;
-    border-color:black;
-    
-}
-.msg{
-    position:relative;
-    width:500px;
-    text-align:center;
-    border-width:1;
-    border-style:solid;
-    border-color:black;
-    background-color:silver;
-    font-family:verdana;
-    font-size:12;
-    font-weight:bold;
-}
-</style>
-
-<?php
 require "conectar.php";
 
 //inicio formulario
 function formulario(){
 
 echo "
-<form method=\"POST\" name=\"cadastro\" onSubmit=\"return verificar()\" action=\"cadastroProtocolo.php\">
+<form method=\"POST\" name=\"cadastro\" onSubmit=\"return verificar()\" action=\"index.php?pagina=Novo\">
 <table border=\"0\" align=center>
 <tr>
   <td class=\"descCampo\" ><label for=\"cpfCnpjCliente\">Cpf/Cnpj:</label></td>
@@ -64,7 +15,7 @@ echo "
 </tr>
 <tr>
   <td class=\"descCampo\" ><label for=\"obs\">Obs:</label></td>
-  <td colspan=4><input type=\"text\" maxlength=\"300\" size=\"78\" name=\"obs\" id=\"obs\"></td>
+  <td colspan=4><input type=\"text\" maxlength=\"300\" size=\"82\" name=\"obs\" id=\"obs\"></td>
 </tr>
 <tr>
    <td colspan=4 align=\"right\"><input type=\"submit\" value=\"Incluir\" name=\"incluir\" >
@@ -72,16 +23,16 @@ echo "
 </table>
 <br>
 
-<p align=\"center\">..........................................................................................................................................................</p>
+<p align=\"center\">...................................................................................................................................</p>
 
 <div>
-<table border=\"0\" width=\"600\" align=\"center\" class=\"tabItemProtocolo\">
+<table border=\"0\" width=\"650\" align=\"center\" class=\"tabItemProtocolo\">
 <thead>
 <tr>
-    <th class=\"descCampo\" >Nome</th>
-    <th class=\"descCampo\" >Cpf/Cnpj</th>
-    <th class=\"descCampo\" width=\"10\">Tipo</th>
-    <th class=\"descCampo\" >Obs</th>
+    <th>Nome</th>
+    <th>Cpf/Cnpj</th>
+    <th width=\"10\">Tipo</th>
+    <th >Obs</th>
 </tr>
 </thead>
 ";
@@ -91,7 +42,8 @@ echo "
             on A.codProtocolo = B.codProtocolo
             where A.codProtocolo ='".$_SESSION['codProtocolo']."'";
         $resultado = mysql_query($sql) or die ("erro sql".mysql_error());
-
+        $total = mysql_num_rows($resultado);
+        $_SESSION['total'] = $total;
         while ($linha = mysql_fetch_array($resultado)){
         echo "
         <tbody>
@@ -104,23 +56,24 @@ echo "
         }
 
 echo "
+    <tr>
+        <th colspan=4>Total Contratos: ".$total."</th>
+    </tr>
+
 </tbody>
 </table>
 </div>
-<br>
-<table border=\"0\"align=\"center\">
+<table border=\"0\" align=\"center\">
 <thead>
     <tr>
-        <td align=\"right\"><input type=\"submit\" alt=\"teste\" value=\"voltar\" name=\"voltar\" >
-        <td align=\"right\"><input type=\"submit\" value=\"gravar\" name=\"gravar\" >
-        <td align=\"right\"><input type=\"submit\" value=\"enviar\" name=\"enviar\" >
+        <td align=\"right\"><input type=\"submit\" value=\"Deletar\" name=\"deletar\" >
+        <td align=\"right\"><input type=\"submit\" value=\"Gravar\" name=\"gravar\" >
+        <td align=\"right\"><input type=\"submit\" value=\"Enviar\" name=\"enviar\" >
     </tr>
 </thead>
 <tbody>
 </tbody>
 </table>
-
-
 </form>";
 }
 //fim formulario
@@ -129,17 +82,16 @@ echo "
 function gravarCabecalho(){
 
 
-if (isset($_SESSION['codProtocolo'])){
-    
+if (isset($_SESSION['codProtocolo']) && !$_SESSION['codProtocolo']==""){
+
     }else{
         $sql2 = "select * from protocolo order by codProtocolo desc limit 1 ";//busca o ultimo cod que está no banco
         $resultado2 = mysql_query($sql2) or die ("erro sqlGravarCabecalho".mysql_error());
         $dado2 = mysql_fetch_assoc($resultado2);
         $codProtocolo = $dado2['codProtocolo']+1;//acrescenta + 1 no codigo que buscou do banco
-
         $_SESSION['codProtocolo'] = $codProtocolo;
 
-        $sql = "INSERT INTO protocolo (codProtocolo,dataCriacao,status,codUsuario,codEmpresa,quantidadeContratos) VALUES ('$codProtocolo',now(),'A','1','1','0')";
+$sql = "INSERT INTO protocolo (codProtocolo,dataCriacao,status,codUsuario,codEmpresa,quantidadeContratos) VALUES ('$codProtocolo',now(),'A','1','1','0')";
         $resultadosql = mysql_query($sql) or die ("erro sql GravarCabecalho 2".mysql_error());
 }
 
@@ -150,31 +102,119 @@ if (isset($_SESSION['codProtocolo'])){
 
 //inicio formulario
 function gravarItemProtocolo(){
-    $nome = $_POST['nome'];
-    $cpfCnpjCliente = $_POST['cpfCnpjCliente'];
-    $obs = $_POST['obs'];
-echo $_SESSION['codProtocolo'];
-    $sql = "INSERT INTO itemprotocolo (cpfCnpjCliente,nomeCliente,tipo,codProtocolo,obs,dataPagamento,documento)
-            VALUES ('$cpfCnpjCliente','$nome','N','".$_SESSION['codProtocolo']."','$obs','0000-00-00','nada')";
+    $_SESSION['nome'] = $_POST['nome'];
+    $_SESSION['cpfCnpjCliente'] = $_POST['cpfCnpjCliente'];
+    $_SESSION['obs'] = $_POST['obs'];
 
-    $resultadosql = mysql_query($sql) or die ("erro sql gravarItemProtocolo".mysql_error());
+    $sql_ver = "select * from itemprotocolo A
+            join
+            protocolo B
+            on A.codProtocolo = B.codProtocolo
+            where A.cpfCnpjCliente ='".$_SESSION['cpfCnpjCliente']."'";
 
-    formulario();
+    $resultado_ver = mysql_query($sql_ver);
+    $linha_ver = mysql_num_rows($resultado_ver);
+
+    if ($linha_ver>0){
+        echo "<div class=msg><b>CPF/Cnpj já possui protocolo</b><br>";
+            while ($linha = mysql_fetch_array($resultado_ver)){
+                echo"Protocolo: ".$linha['codProtocolo']." - Enviado: ".$linha['dataEnvio']."<br>";
+                }
+
+        echo "<br>Deseja enviar como Novo ou Pendência?";
+        echo "
+        <form method=\"POST\" name=\"cadastro\" onSubmit=\"return verificar()\" action=\"index.php?pagina=Novo\">
+        <table border=\"0\" align=\"center\">
+         <thead>
+         <tr>
+            <td align=\"right\"><input type=\"submit\" value=\"Novo\" name=\"novo\" >
+            <td align=\"right\"><input type=\"submit\" value=\"Pendência\" name=\"pendencia\" >
+         </tr>
+         </thead>
+         <tbody>
+         </tbody>
+         </table>
+         </form></div>";
+                 }else{
+                 $sql = "INSERT INTO itemprotocolo (cpfCnpjCliente,nomeCliente,tipo,codProtocolo,obs,dataPagamento,documento)
+                 VALUES ('".$_SESSION['cpfCnpjCliente']."','".$_SESSION['nome']."','N','".$_SESSION['codProtocolo']."','".$_SESSION['obs']."','0000-00-00','nada')";
+                 $resultadosql = mysql_query($sql) or die ("erro sql gravarItemProtocolo".mysql_error());
+                    }
 }
 //fim formulario
 
+function salvarProtocolo(){
+
+$sql = "UPDATE protocolo SET status='S',quantidadeContratos='".$_SESSION['total']."'
+           WHERE codProtocolo = '".$_SESSION['codProtocolo']."' ;";
+$resultadosql = mysql_query($sql) or die ("erro sql salvarFormulario".mysql_error());
+echo "<div class=\"msg\">Protocolo Salvo com sucesso</div>";
+}
+
+function enviarProtocolo(){
+    $sql = "UPDATE protocolo SET status='E',quantidadeContratos='".$_SESSION['total']."', dataEnvio=now(),codUsuario='1',codEmpresa='1'
+    WHERE codProtocolo = '".$_SESSION['codProtocolo']."' ;";
+    $resultadosql = mysql_query($sql) or die ("erro sql salvarFormulario".mysql_error());
+    echo "<div class=\"msg\">Protocolo Enviado<br>
+    <b>Protocolo: ".$_SESSION['codProtocolo']."</b>
+    </div>";
 
 
+$_SESSION['codProtocolo']="";
 
+}
+function gravarItemProtocoloPendencia(){
+    $sql =  $sql = "INSERT INTO itemprotocolo (cpfCnpjCliente,nomeCliente,tipo,codProtocolo,obs,dataPagamento,documento)
+                 VALUES ('".$_SESSION['cpfCnpjCliente']."','".$_SESSION['nome']."','P','".$_SESSION['codProtocolo']."','".$_SESSION['obs']."','0000-00-00','nada')";
+    $resultadosql = mysql_query($sql) or die ("erro sql gravarItemProtocoloPendencia ".mysql_error());
+}
+function gravarItemProtocoloNovo(){
+    $sql =  $sql = "INSERT INTO itemprotocolo (cpfCnpjCliente,nomeCliente,tipo,codProtocolo,obs,dataPagamento,documento)
+                 VALUES ('".$_SESSION['cpfCnpjCliente']."','".$_SESSION['nome']."','N','".$_SESSION['codProtocolo']."','".$_SESSION['obs']."','0000-00-00','nada')";
+    $resultadosql = mysql_query($sql) or die ("erro sql gravarItemProtocoloNovo ".mysql_error());
+}
 
+function deletarProtocolo(){
+    $sql = "DELETE FROM itemProtocolo WHERE codProtocolo='".$_SESSION['codProtocolo']."';";
+    $resultadosql = mysql_query($sql) or die ("erro sql deletarItemProtocolo".mysql_error());
 
-if (!array_key_exists("incluir",$_POST)){
-    formulario();
-    gravarCabecalho();
+    $sql = "DELETE FROM protocolo WHERE codProtocolo='".$_SESSION['codProtocolo']."';";
+    $resultadosql = mysql_query($sql) or die ("erro sql deletarItemProtocolo".mysql_error());
+
+echo "<div class=\"msg\">Protocolo Deletado</div>";
+
+$_SESSION['codProtocolo']="";
+}
+
+if(array_key_exists("enviar", $_POST)){
+    enviarProtocolo();
     }
-    else{
-        gravarItemProtocolo();
+    if(array_key_exists("gravar", $_POST)){
+        salvarProtocolo();
+        formulario();
         }
+        if (array_key_exists("incluir",$_POST)){
+            gravarItemProtocolo();
+            formulario();
 
+            }
+            if(array_key_exists("pendencia", $_POST)){
+              gravarItemProtocoloPendencia();
+              formulario();
+              }
+              if(array_key_exists("novo", $_POST)){
+                gravarItemProtocoloNovo();
+                formulario();
+                }
+                if(array_key_exists("deletar", $_POST)){
+                    deletarProtocolo();
+                    }
+                    if ( !array_key_exists("enviar", $_POST) && !array_key_exists("gravar", $_POST)
+                        && !array_key_exists("incluir",$_POST) && !array_key_exists("pendencia", $_POST)
+                        && !array_key_exists("novo", $_POST) && !array_key_exists("deletar", $_POST)){
+                           formulario();
+                           gravarCabecalho();
+
+                           }
 
 ?>
--- www/index.php
+++ www/index.php
@@ -1,5 +1,17 @@
-<?
-/*
+<? session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Title      : Popular
+Version    : 1.0
+Released   : 20080519
+Description: A two-column, fixed-width and lightweight template ideal for 1024x768 resolutions. Suitable for blogs and small websites.
+
     _____________________________________________________________
     |        SISTEMA PROTOCOLOS PARA DOCUMENTOS FIS�COS         |
     |�����������������������������������������������������������|
@@ -13,11 +25,76 @@
     |�����������������������������������������������������������|
     |1 - Documento                                      [75%]   |
     |2 - Modelo Banco Dados                             [100%]  |
-    |3 - Desenvolvimento                                [16% ]  |
+    |3 - Desenvolvimento                                [45% ]  |
     |4 - Testes/Implementa��o/Documenta��o              [0%  ]  |
     |5 - Banca                                          [0%  ]  |
     |___________________________________________________________|
-*/
+
+-->
+
+
+
+<html>
+<head>
+<title>Popular by Free Css Templates</title>
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!-- inicio menu -->
+<div id="logo">
+
+</div>
+
+<div id="header">
+	<div id="menu">
+		<ul>
+            <li><a href="index.php">Home</a></li>
+			<li><a href="index.php?pagina=Novo">Novo Protocolo</a></li>
+			<li><a href="index.php?pagina=Consulta">Consultar Protocolo</a></li>
+			<li><a href="index.php?pagina=Relatorio">Relatório de Protocolos</a></li>
+          	<li><a href="index.php?pagina=Administrador">Administrador</a></li>
+            <li class="last"><a href="#">Sair</a></li>
+		</ul>
+	</div>
+</div>
+<!-- fim menu -->
+
+<!-- inicio pagina -->
+<div id="page">
+  <p>
+    <?php
+
+    
+    
+    $pagina=$_GET['pagina'];
+    
+    if ($pagina=="Novo"){
+        require ("cadastroProtocolo.php");
+        }
+        if($pagina=="Consulta"){
+            require("consultaProtocolo.php");
+            }
+            if($pagina=="Relatorio"){
+                require("relatorioProtocolo.php");
+                }
+                if($pagina=="Administrador"){
+                    require("indexAdministrador.php");
+                    }
+                    else{
+                    require ("home.php");
+                    }
+
+
+
 
 ?>
+</p>
+</div>
+<!-- fim pagina -->
 
+<!--inicio rodape-->
+<div id="footer">
+	<p id="legal">Desenvolvido por Charles Reitz - 2009</p>
+<!-- fim rodape -->
+</body>
+</html>
