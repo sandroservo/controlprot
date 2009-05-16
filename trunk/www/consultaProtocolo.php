0729f0cf76926024757750eb36236e886e6b6fd6
charles.reitz <charles.reitz@c09b9852-311b-11de-a1c6-114bfcb2beae>
2009-05-16 15:09:07 +0000

    1 - Modifica algumas configuracoes do CSS 2 - Iniciado o arquivo de contultaProtocolo 3 - Feito alguns ajustos no arquivo novoProtocolo X implmentando o botao excluir item

--- trunk/www/consultaProtocolo.php
+++ trunk/www/consultaProtocolo.php
@@ -1,3 +1,132 @@
 <?php
+require 'conectar.php';
+
+
+function formulario(){
+    echo "<form name=\"consulta\" action=\"index.php?pagina=Consulta\" method=\"POST\">
+    <table border=\"0\" align=\"center\">
+    <thead>
+    <tr>
+        <td height=\"30\" class=\"descCampoConsulta\"><input type=\"radio\" name=\"dado\" value=\"codProtocolo\" CHECKED/>Numero Protocolo</td>
+        <td height=\"30\" class=\"descCampoConsulta\"><input type=\"radio\" name=\"dado\" value=\"nomeCliente\">Nome</td>
+        <td height=\"30\" class=\"descCampoConsulta\"><input type=\"radio\" name=\"dado\" value=\"dataEnvio\">Data Envio</td>
+        <td height=\"30\" class=\"descCampoConsulta\"><input type=\"radio\" name=\"dado\" value=\"cpfCnpjCliente\">CPF/CNPJ</td>
+
+</tr>
+    </thead>
+    <tbody>
+    <tr>
+
+        <td colspan=\"4\">
+            Consulta:
+            <input type=\"text\" name=\"campo\" value=\"\" size=\"53\" id=\"campo\">
+            <input type=\"submit\" value=\"Consultar\" name=\"consultar\" />
+        </td>
+    </tr>
+    </tbody>
+    </table></form>";
+
+}
+
+
+
+function consultar($dado,$campo){
+
+    if ($dado=='nome'){
+        $sql = "select cpfCnpjCliente,nomeCliente,codProtocolo,staus,quantidadeContrato from itemProtocolo A
+                join
+                protocolo B
+                on A.codProtocolo = B.codProtocolo
+                where A.nomeCliente like '%$campo%'"; 
+        $resultado = mysql_query($sql) or die ("erro sql".mysql_error());
+        }
+        if ($dado=='codProtocolo'){
+        $sql = "select * from itemProtocolo A
+                join
+                protocolo B
+                on A.codProtocolo = B.codProtocolo
+                where A.$dado ='$campo'
+                group by A.codProtocolo;";
+        $resultado = mysql_query($sql) or die ("erro sql".mysql_error());
+        }
+        else{
+            $sql = "select * from itemProtocolo A
+                join
+                protocolo B
+                on A.codProtocolo = B.codProtocolo
+                where A.$dado = '$campo'";
+            $resultado = mysql_query($sql) or die ("erro sql".mysql_error());
+            }
+
+echo "<div><table width=\"650\" border=\"1\" cellspacing=\"0\" bordercolor=\"black\" align=\"center\" class=\"tabItemProtocolo\">
+<thead>
+<tr >
+<th >Numero Protocolo:</td>
+<th >CPF/CNPJ:</td>
+<th >Data Envio:</td>
+<th >Status</td>
+<th >Qtd Contratos</td>
+<th colspan=\"2\">Opções</td>
+</tr>";
+
+//inicio do while para mostrar resultado da consulta
+    while ($linha = mysql_fetch_array($resultado)){
+        echo "<tr>
+        <td class=\"resultCampo\">".$linha['codProtocolo']."</td>
+        <td class=\"resultCampo\">".$linha['cpfCnpjCliente']."</td>
+        ";
+        if(array_key_exists('dataEnvio', $linha)){
+            echo "<td class=\"resultCampo\">";$linha['dataEnvio'];
+        }else{echo"<td class=\"resultCampo\">Não Enviado</td>";}
+        echo "</td>
+        <td class=\"resultCampo\">".$linha['status']."</td>
+        <td class=\"resultCampo\">".$linha['quantidadeContratos']."</td>
+        <td class=\"resultCampo\"><input type=\"submit\" value=\"Alterar\" name=\"alterar\" /></td>
+        <td><label><a href=\"index.php?pagina=Consulta&tipo=excluir&cod=".$linha['codProtocolo']."\">Excluir</label></td>
+        </tr>";
+        }
+
+$total = mysql_num_rows($resultado);
+
+echo"<tr>
+<th colspan=\"7\">Total de Registros:$total</th>
+</tr>
+</table>
+</div>";
+        
+}
+
+
+
+function deletarProtocolo($codProtocolo){
+    $sql = "DELETE FROM itemProtocolo WHERE codProtocolo='$codProtocolo';";
+    $resultadosql = mysql_query($sql) or die ("erro sql deletarItemProtocolo".mysql_error());
+
+    $sql = "DELETE FROM protocolo WHERE codProtocolo='$codProtocolo';";
+    $resultadosql = mysql_query($sql) or die ("erro sql deletarItemProtocolo".mysql_error());
+
+    echo "<div class=\"msgR\">Protocolo Deletado</div>";
+}
+
+
+$dado = $_POST['dado'];
+$campo = $_POST['campo'];
+
+$tipo = $_GET['tipo'];
+$codProtocolo = $_GET['cod'];
+
+
+if (array_key_exists("consultar",$_POST)){
+    formulario();
+    consultar($dado,$campo);
+    }
+    if ($tipo=="excluir"){
+        deletarProtocolo($codProtocolo);
+        formulario();
+        }
+        if(!$tipo =='excluir' && !array_key_exists("consultar",$_POST)){
+            formulario();
+        }
 
 ?>
+
--- trunk/www/index.php
+++ trunk/www/index.php
@@ -13,20 +13,20 @@ Released   : 20080519
 Description: A two-column, fixed-width and lightweight template ideal for 1024x768 resolutions. Suitable for blogs and small websites.
 
     _____________________________________________________________
-    |        SISTEMA PROTOCOLOS PARA DOCUMENTOS FISï¿½COS         |
-    |ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½|
-    | Elaborado por: Charles Reitz                                 |
+    |        SISTEMA PROTOCOLOS PARA DOCUMENTOS FISï¿½COS        |
+    |ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï½|
+    | Elaborado por: Charles Reitz                              |
     | E-mail/MSN: charles.reitz@gmail.com                       |
     | -> Disciplina de Analise e Desenvolvimento de Sistemas    |
     | -> Prof. MEng. Sigmundo Preissler Jr.                     |
-    | -> UNERJ - Jaraguï¿½ do Sul - SC - www.unerj.br             |
+    | -> UNERJ - Jaraguï¿½ do Sul - SC - www.unerj.br           |
     |___________________________________________________________|
     |                       Etapas                              |
-    |ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½|
+    |ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½|
     |1 - Documento                                      [75%]   |
     |2 - Modelo Banco Dados                             [100%]  |
     |3 - Desenvolvimento                                [45% ]  |
-    |4 - Testes/Implementaï¿½ï¿½o/Documentaï¿½ï¿½o              [0%  ]  |
+    |4 - Testes/Implementaï¿½ï¿½o/Documentaï¿½ï¿½o      [0%  ]  |
     |5 - Banca                                          [0%  ]  |
     |___________________________________________________________|
 
@@ -36,31 +36,9 @@ Description: A two-column, fixed-width and lightweight template ideal for 1024x7
 
 <html>
 <head>
-<title>Popular by Free Css Templates</title>
+<title>Controlprot - Sistema de Controle de Protocolos</title>
 <link href="default.css" rel="stylesheet" type="text/css" />
 </head>
-<script>
-
-    startList = function() {
-if (document.all&&document.getElementById) {
-navRoot = document.getElementById("nav");
-for (i=0; i<navRoot.childNodes.length; i++) {
-node = navRoot.childNodes[i];
-if (node.nodeName=="LI") {
-node.onmouseover=function() {
-this.className+=" over";
-  }
-  node.onmouseout=function() {
-  this.className=this.className.replace
-	(" over", "");
-   }
-   }
-  }
- }
-}
-window.onload=startList;
-</script>
-
 <body>
 <!-- inicio menu -->
 <div id="logo">
@@ -73,7 +51,7 @@ window.onload=startList;
             <li><a href="index.php">Home</a><li>
             <li><a href="index.php?pagina=Novo">Novo Protocolo</a></li>
 			<li><a href="index.php?pagina=Consulta">Consultar Protocolo</a></li>
-			<li><a href="index.php?pagina=Relatorio">RelatÃ³rio de Protocolos</a></li>
+			<li><a href="index.php?pagina=Relatorio">Relatório de Protocolos</a></li>
           	<li><a href="index.php?pagina=Administrador">Administrador</a>
                 
             <li class="last"><a href="#">Sair</a></li>
